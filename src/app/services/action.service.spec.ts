fef6cc2dafdc38f61a1f33f53e33882263b8b48a
Dr. Jochen Manns <dev-2@psimarron.net>
2024-12-26 12:20:11 +0100

    Parse movements.

--- src/app/services/action.service.spec.ts
+++ src/app/services/action.service.spec.ts
@@ -1,6 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ActionService, MessageAction } from './action.service';
+import {
+  ActionService,
+  DeadAction,
+  MessageAction,
+  MoveAction,
+  PickAction,
+} from './action.service';
 
 describe('ActionService', () => {
   let service: ActionService;
@@ -45,7 +51,7 @@ describe('ActionService assign message', () => {
 
     expect(action).toBeInstanceOf(MessageAction);
     expect(action.message).toBe('test');
-    expect(action.optional).toBeFalse();
+    expect(action.silent).toBeFalse();
 
     [actions, index] = service.parse('@message = test', 'single', [], 0);
 
@@ -56,7 +62,7 @@ describe('ActionService assign message', () => {
 
     expect(action).toBeInstanceOf(MessageAction);
     expect(action.message).toBe('test');
-    expect(action.optional).toBeTrue();
+    expect(action.silent).toBeTrue();
 
     [actions, index] = service.parse('', 'single', ['', 'message = test'], 0);
 
@@ -67,7 +73,7 @@ describe('ActionService assign message', () => {
 
     expect(action).toBeInstanceOf(MessageAction);
     expect(action.message).toBe('test');
-    expect(action.optional).toBeFalse();
+    expect(action.silent).toBeFalse();
 
     [actions, index] = service.parse('@message = *', 'multiple', [], 0);
 
@@ -78,7 +84,7 @@ describe('ActionService assign message', () => {
 
     expect(action).toBeInstanceOf(MessageAction);
     expect(action.message).toBe('*');
-    expect(action.optional).toBeTrue();
+    expect(action.silent).toBeTrue();
   });
 
   it('invalid', () => {
@@ -97,7 +103,7 @@ describe('ActionService sequence', () => {
   });
 
   it('valid', () => {
-    const [actions, index] = service.parse(
+    let [actions, index] = service.parse(
       '(message = 1, @message = 2)',
       'none',
       [],
@@ -111,12 +117,177 @@ describe('ActionService sequence', () => {
 
     expect(action).toBeInstanceOf(MessageAction);
     expect(action.message).toBe('1');
-    expect(action.optional).toBeFalse();
+    expect(action.silent).toBeFalse();
 
     action = actions[1] as MessageAction;
 
     expect(action).toBeInstanceOf(MessageAction);
     expect(action.message).toBe('2');
-    expect(action.optional).toBeTrue();
+    expect(action.silent).toBeTrue();
+
+    [actions, index] = service.parse(
+      '',
+      'none',
+      ['', '(', 'message = 1,', '@message = 2', ')'],
+      0
+    );
+
+    expect(index).toBe(4);
+    expect(actions.length).toBe(2);
+
+    action = actions[0] as MessageAction;
+
+    expect(action).toBeInstanceOf(MessageAction);
+    expect(action.message).toBe('1');
+    expect(action.silent).toBeFalse();
+
+    action = actions[1] as MessageAction;
+
+    expect(action).toBeInstanceOf(MessageAction);
+    expect(action.message).toBe('2');
+    expect(action.silent).toBeTrue();
+  });
+
+  it('invalid', () => {
+    expect(() =>
+      service.parse('(message = 1, @message = 2', 'none', [], 0)
+    ).toThrowError(/unterminated/);
+
+    expect(() =>
+      service.parse('(message = 1 @message = 2', 'none', [], 0)
+    ).toThrowError(/comma/);
+  });
+});
+
+describe('ActionService pick something', () => {
+  let service: ActionService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ActionService);
+  });
+
+  it('valid', () => {
+    let [actions, index] = service.parse('<test', 'none', [], 0);
+
+    expect(index).toBe(0);
+    expect(actions.length).toBe(1);
+
+    let action = actions[0] as PickAction;
+
+    expect(action).toBeInstanceOf(PickAction);
+    expect(action.what).toBe('test');
+    expect(action.silent).toBeFalse();
+    expect(action.always).toBeFalse();
+
+    [actions, index] = service.parse('@<test', 'none', [], 0);
+
+    expect(index).toBe(0);
+    expect(actions.length).toBe(1);
+
+    action = actions[0] as PickAction;
+
+    expect(action).toBeInstanceOf(PickAction);
+    expect(action.what).toBe('test');
+    expect(action.silent).toBeTrue();
+    expect(action.always).toBeFalse();
+
+    [actions, index] = service.parse('#<test', 'none', [], 0);
+
+    expect(index).toBe(0);
+    expect(actions.length).toBe(1);
+
+    action = actions[0] as PickAction;
+
+    expect(action).toBeInstanceOf(PickAction);
+    expect(action.what).toBe('test');
+    expect(action.silent).toBeFalse();
+    expect(action.always).toBeTrue();
+
+    [actions, index] = service.parse('@#<test', 'none', [], 0);
+
+    expect(index).toBe(0);
+    expect(actions.length).toBe(1);
+
+    action = actions[0] as PickAction;
+
+    expect(action).toBeInstanceOf(PickAction);
+    expect(action.what).toBe('test');
+    expect(action.silent).toBeTrue();
+    expect(action.always).toBeTrue();
+  });
+
+  it('invalid', () => {
+    expect(() => service.parse('<', 'none', [], 0)).toThrowError(
+      /unterminated/
+    );
+  });
+});
+
+describe('ActionService die', () => {
+  let service: ActionService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ActionService);
+  });
+
+  it('valid', () => {
+    const [actions, index] = service.parse('>>test', 'none', [], 0);
+
+    expect(index).toBe(0);
+    expect(actions.length).toBe(1);
+
+    const action = actions[0] as DeadAction;
+
+    expect(action).toBeInstanceOf(DeadAction);
+    expect(action.reason).toBe('test');
+  });
+
+  it('invalid', () => {
+    expect(() => service.parse('>>', 'none', [], 0)).toThrowError(
+      /unterminated/
+    );
+  });
+});
+
+describe('ActionService move', () => {
+  let service: ActionService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ActionService);
+  });
+
+  it('valid', () => {
+    let [actions, index] = service.parse('>room', 'none', [], 0);
+
+    expect(index).toBe(0);
+    expect(actions.length).toBe(1);
+
+    let action = actions[0] as MoveAction;
+
+    expect(action).toBeInstanceOf(MoveAction);
+    expect(action.area).toBeNull();
+    expect(action.room).toBe('room');
+    expect(action.always).toBeFalse();
+
+    [actions, index] = service.parse('#>$$area$room', 'none', [], 0);
+
+    expect(index).toBe(0);
+    expect(actions.length).toBe(1);
+
+    action = actions[0] as MoveAction;
+
+    expect(action).toBeInstanceOf(MoveAction);
+    expect(action.area).toBe('area');
+    expect(action.room).toBe('room');
+    expect(action.always).toBeTrue();
+  });
+
+  it('invalid', () => {
+    expect(() => service.parse('>', 'none', [], 0)).toThrowError(
+      /unterminated/
+    );
   });
 });
--- src/app/services/action.service.ts
+++ src/app/services/action.service.ts
@@ -5,7 +5,33 @@ export abstract class Action {}
 export class MessageAction extends Action {
   constructor(
     public readonly message: string,
-    public readonly optional: boolean
+    public readonly silent: boolean
+  ) {
+    super();
+  }
+}
+
+export class PickAction extends Action {
+  constructor(
+    public readonly what: string,
+    public readonly silent: boolean,
+    public readonly always: boolean
+  ) {
+    super();
+  }
+}
+
+export class DeadAction extends Action {
+  constructor(public readonly reason: string) {
+    super();
+  }
+}
+
+export class MoveAction extends Action {
+  constructor(
+    public readonly area: string | null,
+    public readonly room: string,
+    public readonly always: boolean
   ) {
     super();
   }
@@ -36,14 +62,37 @@ class Context {
 
 @Injectable({ providedIn: 'root' })
 export class ActionService {
-  private readonly parseMessage = (match: RegExpMatchArray) => {
+  private readonly parseMessage = (
+    match: RegExpMatchArray,
+    context: Context
+  ) => {
+    context.skip(match[0].length);
+
     return [new MessageAction(match[2], !!match[1])];
   };
 
-  private readonly parseList = (match: RegExpMatchArray, context: Context) => {
-    const list: Action[] = [];
+  private readonly parsePick = (match: RegExpMatchArray, context: Context) => {
+    context.skip(match[0].length);
 
-    context.start = match[1];
+    return [new PickAction(match[3], !!match[1], !!match[2])];
+  };
+
+  private readonly parseDead = (match: RegExpMatchArray, context: Context) => {
+    context.skip(match[0].length);
+
+    return [new DeadAction(match[1])];
+  };
+
+  private readonly parseGoto = (match: RegExpMatchArray, context: Context) => {
+    context.skip(match[0].length);
+
+    return [new MoveAction(match[3] ?? null, match[4], !!match[1])];
+  };
+
+  private readonly parseList = (match: RegExpMatchArray, context: Context) => {
+    context.skip(1);
+
+    const list: Action[] = [];
 
     for (;;) {
       context.enforceStart();
@@ -73,8 +122,11 @@ export class ActionService {
     pattern: RegExp,
     generator: (match: RegExpMatchArray, context: Context) => Action[]
   ][] = [
-    [/^\s*(@)?message\s*=\s*([^,)\s]+)/, this.parseMessage],
-    [/^\s*\((.*)$/, this.parseList],
+    [/^\(/, this.parseList],
+    [/^>>([^,)\s]+)/, this.parseDead],
+    [/^(#)?>(\$\$([^$]+)\$)?([^,)\s>]+)/, this.parseGoto],
+    [/^(@)?message\s*=\s*([^,)\s]+)/, this.parseMessage],
+    [/^(@)?(#)?<([^,)\s]+)/, this.parsePick],
   ];
 
   parse(
@@ -105,8 +157,6 @@ export class ActionService {
       const code = generator(match, context);
 
       if (!code.length) throw new Error(`bad action '${context.start}'`);
-
-      context.skip(match[0].length);
 
       return code;
     }
