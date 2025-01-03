a2eb8c89f7f0951cb7ebbb76b7242350e6b8ff28
Dr. Jochen Manns <dev-2@psimarron.net>
2025-01-03 15:00:29 +0100

    Review actions.

--- src/app/actions/parseContext.ts
+++ src/app/actions/parseContext.ts
@@ -23,46 +23,59 @@ import { TestNotPositionAction } from './testNotPosition';
 import { TestPositionAction } from './testPosition';
 import { TestStateAction } from './testState';
 
+/** Signature of a parsable action class. */
 interface IActionParser {
+  /** Static regular expression. */
   readonly Pattern: RegExp;
 
+  /** Static method to create an action instance after a sucessfull regular expression matching. */
   parse(match: RegExpMatchArray, context: ParseContext): Action | Action[];
 }
 
+/** All supported actions. */
 const parsers: IActionParser[] = [
-  ListAction,
   DeadAction,
-  PrintAction,
-  TestMessageAction,
-  TestStateAction,
-  TestPositionAction,
-  TestNotPositionAction,
-  HasAction,
-  NotHasAction,
-  HereAction,
-  NotHereAction,
-  HasThisAction,
-  NotHasThisAction,
-  MoveAction,
-  MessageAction,
-  PickAction,
   DropAction,
+  HasAction,
+  HasThisAction,
+  HereAction,
+  ListAction,
+  MessageAction,
+  MoveAction,
+  NotHasAction,
+  NotHasThisAction,
+  NotHereAction,
+  PickAction,
+  PrintAction,
   RemoveAction,
   ResetAction,
-  StopAction,
   StartAction,
+  StopAction,
+  TestMessageAction,
+  TestNotPositionAction,
+  TestPositionAction,
+  TestStateAction,
   /* Must be last in the indicated order. */
   SetMessageAction,
   CallAction,
 ];
 
+/** Parsing helper. */
 export class ParseContext {
+  /**
+   * Create a new helper instance.
+   *
+   * @param start first line to inspect - typically clipped off at the left.
+   * @param lines all lines.
+   * @param index index of the line with the start text.
+   */
   constructor(
     public start: string,
     public lines: string[],
     public index: number
   ) {}
 
+  /** Merge in the next line into the command to analyse. */
   joinNext() {
     if (++this.index >= this.lines.length)
       throw new Error('unterminated action');
@@ -70,41 +83,60 @@ export class ParseContext {
     this.start = this.lines[this.index];
   }
 
+  /** Make sure the command to analyse is not emtpy. */
   enforceStart() {
     while (!this.start) this.joinNext();
   }
 
+  /**
+   * Clip off characters from the left of the current
+   * command and remove leading spaces.
+   *
+   * @param n Number of characters to get rid off.
+   */
   skip(n: number) {
     this.start = this.start.substring(n).trim();
   }
 
+  /** Try to analyse the current command. */
   parse() {
     for (const parser of parsers) {
+      /** Check command as is. */
       const match = parser.Pattern.exec(this.start);
 
       if (!match) continue;
 
+      /** Create the action from the match and make it an array of actions. */
       const parsed = parser.parse(match, this);
-      const code = parsed && (Array.isArray(parsed) ? parsed : [parsed]);
+      const actions = parsed && (Array.isArray(parsed) ? parsed : [parsed]);
 
-      if (!code.length) throw new Error(`bad action '${this.start}'`);
+      if (!actions.length) throw new Error(`bad action '${this.start}'`);
 
-      return code;
+      /** Report list of actions - typically only one but can be a list as well. */
+      return actions;
     }
 
+    /** No match found. */
     return;
   }
 
+  /**
+   * Parse the body of a conditional statement.
+   *
+   * @param skip prefix detected so far.
+   * @returns actions in the body.
+   */
   parseBody(skip: string): Action[] {
+    /** Skip what we matched so far. */
     this.skip(skip.length);
 
     for (;;) {
-      const code = this.parse();
+      /** Try to get the body. */
+      const actions = this.parse();
 
-      if (code)
-        if (!Array.isArray(code)) return [code];
-        else if (code.length) return code;
+      if (actions?.length) return actions;
 
+      /** Maybe some statements are incomplete so just join the next line. */
       this.joinNext();
     }
   }
