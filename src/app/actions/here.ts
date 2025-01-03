be9df3d8b002a38bcd81674e2e3ce811a94d75c9
Dr. Jochen Manns <dev-2@psimarron.net>
2025-01-03 13:53:26 +0100

    Fixes and reviews.

--- src/app/actions/here.ts
+++ src/app/actions/here.ts
@@ -4,11 +4,20 @@ import { Entity } from '../game-object/entity';
 import { GameService } from '../services/game.service';
 import { ParseContext } from './parseContext';
 
+/** See if some entity is lying around in the current room. */
 export class HereAction extends Action {
+  /** 'if_here' ' ' <entity> */
   public static readonly Pattern = /^if_here\s+([^\s]+)/;
 
+  /** Entity of interest. */
   entity!: Entity;
 
+  /**
+   * Create the action.
+   *
+   * @param what entity of interest.
+   * @param actions actions to execute.
+   */
   private constructor(
     public readonly what: string,
     public readonly actions: Action[]
@@ -28,14 +37,17 @@ export class HereAction extends Action {
   }
 
   override validate(game: GameService, scope: GameObject): void {
+    /** Make sure the entity exists. */
     this.entity = game.objects.findEntity(this.what);
 
+    /** Forward to all actions in body. */
     this.actions.forEach((a) => a.validate(game, scope));
   }
 
   protected override onRun(scope: GameObject, game: GameService): void {
     game.debug(`test ${this.entity.key} to be here`);
 
+    /** See if the entity lies in the current room and proceed accordingly. */
     const here = game.player.carriedObjects.has(game.player.state, this.entity);
 
     if (here) Action.run(this.actions, scope, game);
--- src/app/actions/notHere.ts
+++ src/app/actions/notHere.ts
@@ -4,11 +4,20 @@ import { Entity } from '../game-object/entity';
 import { GameService } from '../services/game.service';
 import { ParseContext } from './parseContext';
 
+/** See if some entity is not lying around in the current room. */
 export class NotHereAction extends Action {
+  /** 'if_nothere' ' ' <entity> */
   public static readonly Pattern = /^if_nothere\s+([^\s]+)/;
 
+  /** Entity of interest. */
   entity!: Entity;
 
+  /**
+   * Create the action.
+   *
+   * @param what entity of interest.
+   * @param actions actions to execute.
+   */
   private constructor(
     public readonly what: string,
     public readonly actions: Action[]
@@ -28,14 +37,17 @@ export class NotHereAction extends Action {
   }
 
   override validate(game: GameService, scope: GameObject): void {
+    /** Make sure the entity exists. */
     this.entity = game.objects.findEntity(this.what);
 
+    /** Forward to all actions in body. */
     this.actions.forEach((a) => a.validate(game, scope));
   }
 
   protected override onRun(scope: GameObject, game: GameService): void {
     game.debug(`test ${this.entity.key} not to be here`);
 
+    /** See if the entity lies in the current room and proceed accordingly. */
     const here = game.player.carriedObjects.has(game.player.state, this.entity);
 
     if (!here) Action.run(this.actions, scope, game);
