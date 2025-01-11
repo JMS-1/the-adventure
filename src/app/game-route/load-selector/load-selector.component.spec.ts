cb9173df779d544090d97ea18a68a587742dbba0
Dr. Jochen Manns <dev-2@psimarron.net>
2025-01-11 16:11:33 +0100

    Fix test.

--- src/app/game-route/load-selector/load-selector.component.spec.ts
+++ src/app/game-route/load-selector/load-selector.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { GameService } from '../../services/game.service';
 import { LoadSelectorComponent } from './load-selector.component';
 
 describe('LoadSelectorComponent', () => {
@@ -7,10 +8,14 @@ describe('LoadSelectorComponent', () => {
   let fixture: ComponentFixture<LoadSelectorComponent>;
 
   beforeEach(async () => {
+    TestBed.overrideComponent(LoadSelectorComponent, { set: { inputs: [] } });
+
     await TestBed.configureTestingModule({
-      imports: [LoadSelectorComponent]
-    })
-    .compileComponents();
+      imports: [LoadSelectorComponent],
+      providers: [
+        { provide: GameService, useValue: { currentSavedStates: [] } },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoadSelectorComponent);
     component = fixture.componentInstance;
