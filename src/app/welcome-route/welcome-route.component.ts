9510d6cb265e1afa81d718a8c752a27c202cf4fe
Dr. Jochen Manns <dev-2@psimarron.net>
2025-01-16 20:21:21 +0100

    Added info link.

--- src/app/welcome-route/welcome-route.component.ts
+++ src/app/welcome-route/welcome-route.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { games, TGame } from '../services/settings.service';
 
 /** Initial display of the game application - mostly to choose a game to play. */
 @Component({
   selector: 'app-welcome-route',
-  imports: [CommonModule, MatButtonModule, RouterModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './welcome-route.component.html',
   styleUrl: './welcome-route.component.scss',
 })
