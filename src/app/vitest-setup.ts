2ca92cf47cbb7d26e972ea8bf41050e62d63df15
Dr. Jochen Manns <dev-2@psimarron.net>
2025-06-13 15:22:21 +0200

    Some more tweaks.

--- src/app/vitest-setup.ts
+++ src/app/vitest-setup.ts
@@ -1,1 +1,9 @@
-export const test = 42;
+import 'zone.js';
+
+import { TestBed } from '@angular/core/testing';
+import * as testing from '@angular/platform-browser/testing';
+
+TestBed.initTestEnvironment(
+  testing.BrowserTestingModule,
+  testing.platformBrowserTesting()
+);
