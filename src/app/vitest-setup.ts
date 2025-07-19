d1211c6c81ae4b2625a12f305b960e6e14fd0dcd
Dr. Jochen Manns <dev-2@psimarron.net>
2025-07-19 12:18:10 +0200

    Trying to get vitest extension running - no success so far but looks a bit better.

--- src/app/vitest-setup.ts
+++ src/app/vitest-setup.ts
@@ -1,9 +1,13 @@
 import 'zone.js';
 
+import { ɵresolveComponentResources as resolver } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import * as testing from '@angular/platform-browser/testing';
+import { beforeAll } from 'vitest';
 
 TestBed.initTestEnvironment(
   testing.BrowserTestingModule,
   testing.platformBrowserTesting()
 );
+
+beforeAll(() => resolver(fetch));
